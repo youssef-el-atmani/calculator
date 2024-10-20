f880a6a79718d177e75144fe7057461530f36052
Youssef El Atmani <shihaabyoussef1447@gmail.com>
2024-10-20 16:11:41 +0000

    Store DOM-Elements in vars for dark-light_altering
    Those vars will be used to switch between the modes

--- dark-light-modes.js
+++ dark-light-modes.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const calculator = document.querySelector('.calculator');
+const modeSwitcher = document.querySelector('.mode img');
+const outerDisplayContainer = document.querySelector('.display');
+const innerDisplayContainer = document.querySelector('.show');
+const operandsList = [...document.querySelectorAll('.operands-column button')];
+const operatorWrappersList = [...document.querySelectorAll('.operator-wrapper')];
+const operatorsList = [...document.querySelectorAll('.operator')];
+const equalBtnWrapper = document.querySelector('.equal-wrapper');
